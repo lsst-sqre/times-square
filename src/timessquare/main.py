af429b96fbaab3fe167610d7d83bb6b1784a072f
Jonathan Sick <jsick@lsst.org>
2023-04-13 18:29:52 -0400

    Configure uvicorn logging for structlog
    This makes the uvicorn logging more consistent with app logs.

--- src/timessquare/main.py
+++ src/timessquare/main.py
@@ -18,7 +18,7 @@ from fastapi.responses import JSONResponse
 from safir.dependencies.arq import arq_dependency
 from safir.dependencies.db_session import db_session_dependency
 from safir.dependencies.http_client import http_client_dependency
-from safir.logging import configure_logging
+from safir.logging import configure_logging, configure_uvicorn_logging
 from safir.middleware.x_forwarded import XForwardedMiddleware
 
 from .config import config
@@ -39,6 +39,7 @@ configure_logging(
     log_level=config.log_level,
     name=config.logger_name,
 )
+configure_uvicorn_logging(config.log_level)
 
 app = FastAPI(
     title="Times Square",
