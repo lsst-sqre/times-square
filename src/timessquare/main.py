afb38042cecb131ee957730c6a446a7c86a3b289
Jonathan Sick <jsick@lsst.org>
2023-07-21 15:47:05 -0400

    Adopt new FastAPI lifespan

--- src/timessquare/main.py
+++ src/timessquare/main.py
@@ -9,6 +9,8 @@ called.
 
 from __future__ import annotations
 
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 from importlib.metadata import version
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -20,6 +22,7 @@ from safir.dependencies.db_session import db_session_dependency
 from safir.dependencies.http_client import http_client_dependency
 from safir.logging import configure_logging, configure_uvicorn_logging
 from safir.middleware.x_forwarded import XForwardedMiddleware
+from structlog import get_logger
 
 from .config import config
 from .dependencies.redis import redis_dependency
@@ -32,6 +35,35 @@ if TYPE_CHECKING:
     from fastapi import Request
 
 __all__ = ["app", "config"]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator:
+    """Context manager for the application lifespan."""
+    logger = get_logger("ook")
+    logger.debug("Times Square is starting up.")
+
+    await db_session_dependency.initialize(
+        config.database_url, config.database_password.get_secret_value()
+    )
+    await redis_dependency.initialize(config.redis_url)
+    await arq_dependency.initialize(
+        mode=config.arq_mode, redis_settings=config.arq_redis_settings
+    )
+
+    logger.info("Times Square started up.")
+
+    yield
+
+    # Shutdown phase:
+
+    logger.debug("Times Square is shutting down.")
+
+    await http_client_dependency.aclose()
+    await db_session_dependency.aclose()
+    await redis_dependency.close()
+
+    logger.info("Times Square shut down complete.")
 
 
 configure_logging(
@@ -49,6 +81,7 @@ app = FastAPI(
     docs_url=f"{config.path_prefix}/docs",
     redoc_url=f"{config.path_prefix}/redoc",
     openapi_tags=[{"name": "v1", "description": "Times Square v1 REST API"}],
+    lifespan=lifespan,
 )
 """The FastAPI application for times-square."""
 
@@ -59,24 +92,6 @@ app.add_middleware(XForwardedMiddleware)
 app.include_router(internal_router)
 app.include_router(external_router, prefix=f"{config.path_prefix}")
 app.include_router(v1_router, prefix=f"{config.path_prefix}/v1")
-
-
-@app.on_event("startup")
-async def startup_event() -> None:
-    await db_session_dependency.initialize(
-        config.database_url, config.database_password.get_secret_value()
-    )
-    await redis_dependency.initialize(config.redis_url)
-    await arq_dependency.initialize(
-        mode=config.arq_mode, redis_settings=config.arq_redis_settings
-    )
-
-
-@app.on_event("shutdown")
-async def shutdown_event() -> None:
-    await http_client_dependency.aclose()
-    await db_session_dependency.aclose()
-    await redis_dependency.close()
 
 
 @app.exception_handler(TimesSquareError)
